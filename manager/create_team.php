8e406e85c17ae8f2a2f62b6aec348c16a35c2c77
Neeraj Kumar <neekum73@gmail.com>
2025-06-06 19:09:38 -0400

    upload folder

--- manager/create_team.php
+++ manager/create_team.php
@@ -6,3 +6,9 @@ $team_name = $_POST['team_name'];
 $college = $_POST['college'];
 $user_id = $_SESSION['user_id'];
 $logo_path = "";
+// Handle logo upload if present
+if (!empty($_FILES['logo']['name'])) {
+    $upload_dir = "../uploads/";
+    if (!is_dir($upload_dir)) {
+        mkdir($upload_dir, 0777, true); // create uploads folder if it doesn't exist
+    }
