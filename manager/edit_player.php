00bda555af6992cea8a60ee731dc5ff19a5b3b6c
Neeraj Kumar <neekum73@gmail.com>
2025-06-07 00:03:18 -0400

    getting id from database

--- manager/edit_player.php
+++ manager/edit_player.php
@@ -6,3 +6,6 @@ if ($_SESSION['role'] !== 'team_manager') {
     header("Location: ../login.php");
     exit;
 }
+
+$player_id = $_GET['id'];
+$player = $conn->query("SELECT * FROM players WHERE id = $player_id")->fetch_assoc();
