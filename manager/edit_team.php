8cbc17d1cfe6fc28f60e1744d229724ea9c0f9d1
Neeraj Kumar <neekum73@gmail.com>
2025-06-07 23:18:29 -0400

    created edit team

--- manager/edit_team.php
+++ manager/edit_team.php
@@ -6,3 +6,6 @@ if ($_SESSION['role'] !== 'team_manager') {
     header("Location: ../login.php");
     exit;
 }
+
+$user_id = $_SESSION['user_id'];
+$team = $conn->query("SELECT * FROM teams WHERE user_id = $user_id")->fetch_assoc();
