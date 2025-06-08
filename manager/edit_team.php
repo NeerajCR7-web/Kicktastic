187c07c0b7d0d0c892f6070aba87a981bbd4ab27
Neeraj Kumar <neekum73@gmail.com>
2025-06-07 23:19:03 -0400

    fetch data from database

--- manager/edit_team.php
+++ manager/edit_team.php
@@ -9,3 +9,8 @@ if ($_SESSION['role'] !== 'team_manager') {
 
 $user_id = $_SESSION['user_id'];
 $team = $conn->query("SELECT * FROM teams WHERE user_id = $user_id")->fetch_assoc();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $team_name = $_POST['team_name'];
+    $college = $_POST['college'];
+    $logo_path = $team['logo_url'];
